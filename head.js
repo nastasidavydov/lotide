81e0419fcb12766fde41a563db30f313ed24c110
Nastasi Davydov <nastasidavydov@gmail.com>
2022-01-11 00:32:38 +0000

    Add func that outputs first el of an array which is tested by assertEqual func

---
+++ head.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+  let passed =  '✅✅✅';
+  let failed = '❗❗❗';
+  if (actual === expected) {
+    console.log(`${passed} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${failed} Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = arr => {
+  return arr[0];
+}
+
+assertEqual(head([]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
