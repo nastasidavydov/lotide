06da42211dc5a8fc6a3097ff9cfd9ca2428bcec9
Nastasi Davydov <nastasidavydov@gmail.com>
2022-01-12 01:03:50 +0000

    Create func that checks if every element of 2 arr match

---
+++ eqArrays.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  let passed =  '✅✅✅';
+  let failed = '❗❗❗';
+  if (actual === expected) {
+    console.log(`${passed} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${failed} Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = (arr1, arr2) => {
+
+  if (arr1.length !== arr2.length){
+    return false;
+  }
+      
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
