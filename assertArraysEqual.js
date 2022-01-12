a3c019abfec86593878ea3230f464cea9296b86d
Nastasi Davydov <nastasidavydov@gmail.com>
2022-01-12 01:13:47 +0000

    Create function that checks equality assertion of 2 arr and prints corresponding msg

---
+++ assertArraysEqual.js
@@ -0,0 +1,29 @@
+const eqArrays = (arr1, arr2) => {
+
+  if (arr1.length !== arr2.length){
+    return false;
+  }
+      
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  let passed =  '✅✅✅';
+  let failed = '❗❗❗';
+  if (eqArrays(actual, expected)) {
+    console.log(`${passed} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${failed} Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
+
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
