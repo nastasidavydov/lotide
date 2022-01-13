da53ccde53262cb9c354523d58142d3521ff9e64
Nastasi Davydov <nastasidavydov@gmail.com>
2022-01-13 21:28:03 +0000

    Create a function that search for a key in a given obj which satisfies the provided condition

---
+++ findKey.js
@@ -0,0 +1,52 @@
+const assertEqual = function(actual, expected) {
+  let passed =  '✅✅✅';
+  let failed = '❗❗❗';
+  if (actual === expected) {
+    console.log(`${passed} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${failed} Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = (obj, callback) => {
+  const keys = Object.keys(obj);
+  
+  for (let key of keys) {
+    if (callback(obj[key])) {
+      return key;
+    }
+  }
+};
+
+
+
+let result1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2); // => "noma"
+
+let result2 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3);
+
+let result3 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 4);
+
+assertEqual(result1, 'noma');
+assertEqual(result2, 'Akaleri');
+assertEqual(result3, undefined);
