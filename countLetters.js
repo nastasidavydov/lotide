dbc36b00593a2c22ec6312b7474e0690260c6f55
Nastasi Davydov <nastasidavydov@gmail.com>
2022-01-13 00:06:50 +0000

    Create a func that takes in a string and returns count of each letter of that string

---
+++ countLetters.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  let passed =  '✅✅✅';
+  let failed = '❗❗❗';
+  if (actual === expected) {
+    console.log(`${passed} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${failed} Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = sentence => {
+  const result = {};
+  let letters = sentence.toLowerCase().split("");
+  for (let letter of letters) {
+    if (letter !== ' ') {
+      if (result[letter]) {
+        result[letter] += 1;
+      } else {
+        result[letter] = 1;
+      }
+    }
+  }
+  return result;
+};
+
+
+let outputLetters1 = countLetters('LHL');
+let outputLetters2 = countLetters("lighthouse in the house");
+
+assertEqual(outputLetters1['l'], 2);
+assertEqual(outputLetters2['h'], 4);
