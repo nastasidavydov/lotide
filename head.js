72aa3c5f601983a6074928f4d6dd44fee1cda4a9
Nastasi Davydov <nastasidavydov@gmail.com>
2022-01-17 14:38:03 +0000

    Add import statement of assertEqual func

--- head.js
+++ head.js
@@ -1,12 +1,4 @@
-const assertEqual = function(actual, expected) {
-  let passed =  '✅✅✅';
-  let failed = '❗❗❗';
-  if (actual === expected) {
-    console.log(`${passed} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`${failed} Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const head = arr => {
   return arr[0];
