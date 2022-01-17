da6555701c387befd87d4421b5b8b63bb6778db1
Nastasi Davydov <nastasidavydov@gmail.com>
2022-01-17 21:52:37 +0000

    Create a function that exports all of the functions in lotide

---
+++ index.js
@@ -0,0 +1,37 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects')
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const letterPosition = require('./letterPosition');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
+module.exports = {
+  head,
+  tail,
+  middle,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  assertArraysEqual,
+  assertEqual,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  flatten,
+  letterPosition,
+  map,
+  takeUntil,
+  without
+};
