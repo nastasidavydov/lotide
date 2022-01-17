a6ed5c6f067767cedbd5a8d4eaec0ea796698676
Nastasi Davydov <nastasidavydov@gmail.com>
2022-01-17 14:56:06 +0000

    Export head function

--- head.js
+++ head.js
@@ -1,8 +1,6 @@
-const assertEqual = require('./assertEqual');
-
 const head = arr => {
   return arr[0];
 }
 
-assertEqual(head([]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+module.exports = head;
