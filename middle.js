3e5216d9a7cb6d90754e740504858d36d706cb5b
Nastasi Davydov <nastasidavydov@gmail.com>
2022-01-17 19:25:51 +0000

    Add export statement of middle function

--- middle.js
+++ middle.js
@@ -1,28 +1,3 @@
-const eqArrays = (arr1, arr2) => {
-
-  if (arr1.length !== arr2.length){
-    return false;
-  }
-      
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  let passed =  '✅✅✅';
-  let failed = '❗❗❗';
-  if (eqArrays(actual, expected)) {
-    console.log(`${passed} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`${failed} Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-
 const middle = function(arr) {
   
   let middleInd = Math.floor(arr.length / 2);
@@ -36,15 +11,4 @@ const middle = function(arr) {
   }
 };
 
-console.log(middle([1])) // => []
-console.log(middle([1, 2])) // => []
-
-console.log(middle([1, 2, 3])) // => [2]
-console.log(middle([1, 2, 3, 4, 5])) // => [3]
-
-console.log(middle([1, 2, 3, 4])) // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
-
-assertArraysEqual(middle([5, 3, 4, 6, 7, 6, 5]), [6])
-
-assertArraysEqual(middle([5, 3, 25, 14, 17, 6, 5, 9]), [14, '17'])
+module.exports = middle;
