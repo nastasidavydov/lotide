f69cce0abb99a07e4272e84266eb9f0c2f088144
Nastasi Davydov <nastasidavydov@gmail.com>
2022-01-17 19:14:58 +0000

    Add export statement of assertArraysEqual function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -10,8 +10,4 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
-
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
+module.exports = assertArraysEqual;
