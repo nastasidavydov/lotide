48602e337f1bcb277ff22c3fc970a385bb59d676
Nastasi Davydov <nastasidavydov@gmail.com>
2022-01-17 18:41:37 +0000

    Add export statement of tail function

--- tail.js
+++ tail.js
@@ -1,17 +1,5 @@
-const assertEqual = function(actual, expected) {
-  let passed =  '✅✅✅';
-  let failed = '❗❗❗';
-  if (actual === expected) {
-    console.log(`${passed} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`${failed} Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = arr => {
   return arr.slice(1);
 };
 
-const resultArr = tail(["Hello", "Lighthouse", "Labs"]);
-
-assertEqual(resultArr.length, 3);
+module.exports = tail;
